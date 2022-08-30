fb2484c9bac9a43647b2e8469d244e46d2165b71
khongdudinhdoikhongne <111100815+khongdudinhdoikhongne@users.noreply.github.com>
2022-08-30 16:59:24 +0700

    Update and rename cryptocurrencyPriceWidget.js to cryptocurrencyPWidget.js

---
+++ js/cryptocurrencyPWidget.js
@@ -0,0 +1,17 @@
+let hasCoin = 0, listCryptocurrency = [];
+if(document.querySelector('#coin-is-here')){
+    hasCoin = 1;
+    listCryptocurrency = JSON.parse(document.querySelector('#coin-is-here').getAttribute('data'))
+} else {
+    listCryptocurrency = document.querySelector('h1.pTtl.aTtl.sml.itm').innerText.split('$');
+    listCryptocurrency.shift();
+    listCryptocurrency = document.querySelector('#cryptocurrencyPrices').innerHTML += '<style>.drK .nomics-ticker-widget-bg-light{background-color: #4d4e51} .nomics-ticker-widget-embedded:not(:last-child) {margin-bottom: 20px;} .nomics-ticker-widget-embedded.nomics-ticker-widget-size-responsive{max-width: 100%;}</style>';
+    listCryptocurrency = (listCryptocurrency.map(symbol => symbol.split(' ')[0]))
+    hasCoin = listCryptocurrency.length;
+}
+if(hasCoin){
+    for (let symbol of listCryptocurrency) {
+            document.querySelector('#cryptocurrencyPrices').innerHTML += '<div class="nomics-ticker-widget" data-base="' +symbol+ '" data-quote="USD"/>' }
+            /* ' + ((localStorage.getItem('webMode') === 'drK') ? 'data-theme="dark"' : '') + ' */
+    ldJs('https://widget.nomics.com/embed.js', "cryptocurrency-widget", !0, 'body');
+}

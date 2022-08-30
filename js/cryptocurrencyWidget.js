5574010a935bac9920d6f79c52f922bec8c735d6
khongdudinhdoikhongne <111100815+khongdudinhdoikhongne@users.noreply.github.com>
2022-08-30 16:11:49 +0700

    Update cryptocurrencyWidget.js

--- js/cryptocurrencyWidget.js
+++ js/cryptocurrencyWidget.js
@@ -1,9 +1,9 @@
 const listCryptocurrency = document.querySelector('h1.pTtl.aTtl.sml.itm').innerText.split('$');
 listCryptocurrency.shift();
 if(listCryptocurrency.length){
-    document.querySelector('#cryptocurrencyPrices').innerHTML += '<style>.nomics-ticker-widget-embedded:not(:last-child) {margin-bottom: 20px;} .nomics-ticker-widget-embedded.nomics-ticker-widget-size-responsive{max-width: 100%;}</style>';
+    document.querySelector('#cryptocurrencyPrices').innerHTML += '<style>.drK .nomics-ticker-widget-bg-light{background-color: #4d4e51} .nomics-ticker-widget-embedded:not(:last-child) {margin-bottom: 20px;} .nomics-ticker-widget-embedded.nomics-ticker-widget-size-responsive{max-width: 100%;}</style>';
     for (let symbol of (listCryptocurrency.map(symbol => symbol.split(' ')[0]))) {
-        document.querySelector('#cryptocurrencyPrices').innerHTML += '<div class="nomics-ticker-widget" data-base="' +symbol+ '" data-quote="USD"' + ((localStorage.getItem('webMode') === 'drK') ? 'data-theme="dark"'
-     : '') + '/>' }
+        document.querySelector('#cryptocurrencyPrices').innerHTML += '<div class="nomics-ticker-widget" data-base="' +symbol+ '" data-quote="USD"/>' }
+        /* ' + ((localStorage.getItem('webMode') === 'drK') ? 'data-theme="dark"' : '') + ' */
 }
 ldJs('https://widget.nomics.com/embed.js', "cryptocurrency-widget", !0, 'body');
